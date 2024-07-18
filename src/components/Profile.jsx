1a9251bfdc077848601c1182f8dd5906d1a5a016
Hi, I'm Balikis Omolewa <106351278+Balikis-omolewa@users.noreply.github.com>
2024-07-18 04:31:43 +0100

    Update Profile.jsx
    profile initial commit

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -27,7 +27,7 @@ const MyProfile = ({ changeState }) => {
     <Card className='card'>
       <Card.Img variant="top" width="100%" className='img' src="https://myportfolio-liart-delta.vercel.app/img/hr-bg.png" />
       {showProfile ? (
-        <Card.Body className='card-body'>
+        <Card.Body>
         <Card.Text>{profileData.name}</Card.Text>
         <Card.Title className='title'>{profileData.profession}</Card.Title>
         <Card.Text className='card-text'>{profileData.about}</Card.Text>
@@ -38,7 +38,7 @@ const MyProfile = ({ changeState }) => {
       ) : (
         <p className='p'>Profile Hidden</p>
       )}
-      <Card.Body className='card-body'>
+      <Card.Body>
        <Button variant="primary w-100" className='btn' onClick={handleShowProfile}>Show Profile Details</Button><br />
        <Button variant="primary w-100" className='btn btn-hide' onClick={handleHideProfile}>Hide Profile Details</Button>
        </Card.Body>
